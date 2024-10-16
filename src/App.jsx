dfac84d515a234a9f5f0ad32d976a34f37ebd406
shoyonp <shoyonkumar143@gmail.com>
2024-10-17 01:58:14 +0600

    event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function handleClick() {
+    alert("button clicked");
+  }
+
+  const handleCLick2 = () => {
+    alert("button 2 clicked");
+  };
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>React Core Concepts</h3>
+      {/* <button onclick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleCLick2}>CLick2</button>
+      <button
+        onClick={() => {
+          alert("third clicked");
+        }}
+      >
+        third
+      </button>
+      <button onClick={() => addToFive(3)}>four</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
